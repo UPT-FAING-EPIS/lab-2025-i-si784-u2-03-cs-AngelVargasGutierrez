9e1470e7c17a882df098ee8623a922e37b8c25a4
Angel Vargas Gutierrez <av923488264@gmail.com>
2025-05-29 21:08:06 -0500

    version 1

---
+++ Bank.Domain.Tests/BankAccountTests.cs
@@ -0,0 +1,34 @@
+using Bank.Domain;
+using NUnit.Framework;
+
+namespace Bank.Domain.Tests
+{
+    public class BankAccountTests
+    {
+        [Test]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            // Act
+            account.Debit(debitAmount);
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+
+        [Test]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            // Act and assert
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+        }
+    }
+} 
